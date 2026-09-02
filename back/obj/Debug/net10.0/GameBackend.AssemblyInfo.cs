f2e7e2427e4e49aabbb8902a91ee3e9b71c4bbd1
IIalexandrII <sasha.shunkov@mail.ru>
2026-09-02 22:20:41 +0700

    add gitignore and dockerfile

--- back/obj/Debug/net10.0/GameBackend.AssemblyInfo.cs
+++ back/obj/Debug/net10.0/GameBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b2940b2721248b282a3b231dbaa562c8eaddc0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
